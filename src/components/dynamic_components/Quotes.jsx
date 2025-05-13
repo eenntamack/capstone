53cdd9ad04c9ca7df3500a25a9808d1a4936713b
entamack1511 <entamack1511@gmail.com>
2025-05-13 17:41:51 -0400

    added Quotes component to render dynamic request of the quotes api

--- src/components/dynamic_components/Quotes.jsx
+++ src/components/dynamic_components/Quotes.jsx
@@ -1,19 +1,22 @@
 import { useEffect,useState } from "react"
 import axios from "axios"
 export default function Quotes(){
-    const [quote, setQuote] = useState("")
+    const [quote, setQuote] = useState({q:"",a:""})
     useEffect(()=>{
-        axios.get("https://zenquotes.io/api/quotes").then(res=>{
-            if(res){
-                setQuote(res[0])
+        axios.get("http://localhost:3000/fetchQuote").then(res=>{
+            if(res.data.data){
+                console.log(res.data.data[0].q)
+                setQuote({q:`${res.data.data[0].q}`,a:`${res.data.data[0].a}`})
+            }else{
+                setQuote("")
             }
         })
     },[])
     return(
         <div>
 
-            <p>quote.q</p>
-            <p>quote.a</p>
+            <p>{quote.q}</p>
+            <p>{quote.a}</p>
         </div>
     )
 }
