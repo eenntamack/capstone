1ed68ea7bc1b2db579d315eb49aee9c9d3949121
entamack1511 <entamack1511@gmail.com>
2025-05-15 09:23:21 -0400

    change in vite's handling of reload and redirects

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
+// https://vite.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // increase chunk size limit to 1000 KB
-    outDir: 'build' // optional: make output folder match 'build' if Render expects it
+    chunkSizeWarningLimit: 1000,
+    outDir: 'build'
+  },
+  // important for dev but also reinforces the idea of fallback
+  server: {
+    historyApiFallback: true
   }
 })
